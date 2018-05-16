92e5ffcb69a031368ebb116814c7ebcff04c5a02
Rico <ricodean@hotmail.com>
2018-05-15 23:53:43 -0400

    First Commit

---
+++ TourGuideSolution/TourGuide/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TourGuide.Startup))]
+namespace TourGuide
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
